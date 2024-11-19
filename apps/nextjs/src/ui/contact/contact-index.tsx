8d140857296512b384f738acefc02e0ff7048be2
Piotr Chowaniec <piter.chowaniec@gmail.com>
2024-11-19 09:19:59 +0100

    Fix nexjs seed action cache

--- apps/nextjs/src/ui/contact/contact-index.tsx
+++ apps/nextjs/src/ui/contact/contact-index.tsx
@@ -1,16 +1,27 @@
 "use client";
 
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { getQueryClient } from "~/get-query-client";
 import { seedContactsServerFn } from "~/server/queries";
 import { useGetMyContactsQueryOptions } from "~/server/queryOptions";
 
 import { SeedButton } from "../contacts-sidebar/contacts-seed-button";
 
 export function ContactDefaultPage() {
+  const queryClient = getQueryClient();
+
   const contactsQuery = useSuspenseQuery(useGetMyContactsQueryOptions());
   const { contacts } = contactsQuery.data;
 
   const noContacts = !contacts.length;
+
+  const handleSeed = async () => {
+    await seedContactsServerFn();
+
+    await queryClient.invalidateQueries({
+      queryKey: ["contact"],
+    });
+  };
 
   return (
     <div className="flex w-full flex-col items-center pt-10">
@@ -21,7 +32,7 @@ export function ContactDefaultPage() {
           <hr className="my-8 h-px w-2/6 border-0 bg-gray-700" />
           <p>You can also seed database with test data</p>
 
-          <form action={seedContactsServerFn}>
+          <form action={handleSeed}>
             <SeedButton />
           </form>
         </>
